641634583f3ff6181a23b95f9fc8ff8eb7bbdc4a
xunuwu <xunuwu@gmail.com>
2025-01-16 07:58:46 +0100

    change completion keybindings
    using enter didnt work well with completion in cmdbar or search

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -76,25 +76,14 @@
       enable = true;
       settings = {
         appearance.use_nvim_cmp_as_default = true;
-        completion.documentation = {
-          auto_show = true;
-          auto_show_delay_ms = 250;
+        completion = {
+          documentation = {
+            auto_show = true;
+            auto_show_delay_ms = 250;
+          };
+          trigger.show_in_snippet = false;
         };
-        keymap = {
-          "<C-space>" = ["show" "show_documentation" "hide_documentation"];
-          "<C-e>" = ["hide" "fallback"];
-          "<CR>" = ["accept" "fallback"];
-
-          "<Tab>" = ["snippet_forward" "fallback"];
-          "<S-Tab>" = ["snippet_backward" "fallback"];
-
-          "<C-n>" = ["select_next" "fallback"];
-          "<C-p>" = ["select_prev" "fallback"];
-          "<Up>" = ["scroll_documentation_up" "fallback"];
-          "<Down>" = ["scroll_documentation_down" "fallback"];
-
-          "<C-y>" = ["select_and_accept"];
-        };
+        keymap.preset = "super-tab";
         signature = {
           enabled = true;
         };
