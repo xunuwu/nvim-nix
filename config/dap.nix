5ca1e2a30af7ef82fc2c46a2cb3b08aacfddee1f
xunuwu <xunuwu@gmail.com>
2025-02-04 23:47:37 +0100

    fix renamed options

--- config/dap.nix
+++ config/dap.nix
@@ -1,41 +1,41 @@
 {
   # TODO dap-virtual-text
-  plugins.dap = {
-    enable = true;
-    extensions = {
-      dap-virtual-text.enable = true;
-      dap-ui.enable = true;
-    };
-    adapters.executables = {
-      gdb = {
-        command = "gdb";
-        args = ["-i" "dap"];
+  plugins = {
+    dap-virtual-text.enable = true;
+    dap-ui.enable = true;
+    dap = {
+      enable = true;
+      adapters.executables = {
+        gdb = {
+          command = "gdb";
+          args = ["-i" "dap"];
+        };
+        lldb = {
+          command = "lldb-dap";
+        };
       };
-      lldb = {
-        command = "lldb-dap";
+      configurations = {
+        c = [
+          {
+            name = "Launch (gdb)";
+            type = "gdb";
+            request = "launch";
+            program.__raw = ''
+              function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end
+            '';
+            cwd = "\${workspaceFolder}";
+          }
+          {
+            name = "Launch (lldb)";
+            type = "lldb";
+            request = "launch";
+            program.__raw = ''
+              function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end
+            '';
+            cwd = "\${workspaceFolder}";
+          }
+        ];
       };
-    };
-    configurations = {
-      c = [
-        {
-          name = "Launch (gdb)";
-          type = "gdb";
-          request = "launch";
-          program.__raw = ''
-            function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end
-          '';
-          cwd = "\${workspaceFolder}";
-        }
-        {
-          name = "Launch (lldb)";
-          type = "lldb";
-          request = "launch";
-          program.__raw = ''
-            function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end
-          '';
-          cwd = "\${workspaceFolder}";
-        }
-      ];
     };
   };
   # plugins.hydra.hydras = [
